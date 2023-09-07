f321506302b686a61806bc45b5b0768644635b01
Stephen Vaisey <vaise@TTS-140389WD.win.duke.edu>
2023-09-07 11:20:40 -0400

    dplyr initial demo

---
+++ demos/day-04.R
@@ -0,0 +1,40 @@
+library(tidyverse)
+library(nycflights13)
+
+data(flights)
+
+table(flights$dest)
+table(flights$origin)
+table(flights$dest, 
+      flights$origin)
+
+flights |> count(origin, dest)
+
+flights |> 
+  group_by(origin) |> 
+  summarize(count = n())
+
+flights |> 
+  summarize(count = n())
+
+flights |>
+  group_by(dest) |> 
+  summarize(count = n())
+
+d_numflights <- flights |> 
+  group_by(origin) |> 
+  summarize(nflights = n())
+
+data(mtcars)
+mtcars_tbl <- as_tibble(mtcars, 
+                        rownames = "car")
+
+flights <- flights |>
+  mutate(late = if_else(dep_delay > 0, 1L, 0L))
+
+flights |>
+  drop_na() |> 
+  group_by(carrier) |> 
+  summarize(prop_late = mean(late)) |> 
+  arrange(desc(prop_late))
+
